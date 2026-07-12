87f905b919b8df7304d1b8bb48fcf151c95861bf
samuel-bommer <samuel.bommer@gmail.com>
2026-07-12 17:00:54 +0200

    new exercise done

---
+++ little-sisters-vocab/strings.py
@@ -0,0 +1,81 @@
+"""Functions for creating, transforming, and adding prefixes to strings."""
+
+
+def add_prefix_un(word):
+    """Take the given word and add the 'un' prefix.
+
+    Parameters:
+        word (str): The root word.
+
+    Returns:
+        str: Root word prepended with 'un'.
+    """
+
+    return str('un') + word
+
+def make_word_groups(vocab_words):
+    """Transform a list containing a prefix and words.
+
+    Parameters:
+        vocab_words (list[str]): Vocabulary words with prefix at first index.
+
+    Returns:
+        str: Prefix followed by vocabulary words with prefix applied.
+
+    This function takes a `vocab_words` list of strings and returns a string
+    with the prefix and the words with prefix applied, separated by ' :: '.
+
+    Examples:
+        >>> list('en', 'close', 'joy', 'lighten')
+        'en :: enclose :: enjoy :: enlighten'.
+
+    """
+
+    sep = ' :: ' + vocab_words[0]
+    return sep.join(vocab_words)
+
+def remove_suffix_ness(word):
+    """Remove the suffix from the word while keeping spelling in mind.
+
+    Parameters:
+        word (str): Word to remove suffix from.
+
+    Returns:
+        str: Word with suffix removed & spelling adjusted.
+
+    Examples:
+        >>> remove_suffix_ness('heaviness')
+        'heavy'
+
+        >>> remove_suffix_ness('sadness')
+        'sad'
+
+    """
+
+    word_without_suf = word.removesuffix('ness')
+    if word_without_suf.endswith('i'):
+        word_without_suf = word_without_suf[:-1] + 'y'
+    return word_without_suf
+    
+
+def adjective_to_verb(sentence, index):
+    """Change the adjective within the sentence to a verb.
+
+    Parameters:
+        sentence (str): The word used in a sentence as an adjective.
+        index (int): Index of the adjective to remove and transform.
+
+    Returns:
+        str: The extracted adjective in verb form.
+
+    Examples:
+        >>> adjective_to_verb('It got dark as the sun set.', 2)
+        'darken'
+
+        >>> adjective_to_verb('The ink stains her fingers black.', -1)
+        'blacken'
+
+    """
+    sentence = sentence.removesuffix('.')
+    sentence_list = sentence.split()
+    return sentence_list[index] + 'en'
