25804023f56af7c312beabb751e0fca933a5daf7
samuel-bommer <samuel.bommer@gmail.com>
2026-08-02 13:18:18 +0200

    handshake exercise

--- secret-handshake/secret_handshake.py
+++ secret-handshake/secret_handshake.py
@@ -3,5 +3,34 @@ Secret Handshake
 """
 
 
-def commands(binary_str):
-    pass
+def commands(binary_str: str) -> list:
+    """
+    From binary int
+    """
+    
+    actions = [
+        'wink',
+        'double blink',
+        'close your eyes',
+        'jump'
+    ]
+    action_list = []
+    
+    for index, binary in enumerate(reversed(binary_str)):
+        if index <= 3:
+
+            if binary == '0':
+                continue
+            
+            else:
+                action = actions[index]
+                action_list.append(action)
+        
+        else:
+            break
+            
+    if binary_str[0] == '1':
+        action_list.reverse()
+            
+    return action_list
+    
