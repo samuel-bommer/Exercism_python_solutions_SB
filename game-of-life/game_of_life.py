3a69100060f1b396eeb9e83f721dac5e7a8a2eff
samuel-bommer <samuel.bommer@gmail.com>
2026-08-04 23:37:01 +0200

    game of life beginning

---
+++ game-of-life/game_of_life.py
@@ -0,0 +1,61 @@
+"""
+Game of life
+"""
+
+def determine_neighboors(matrix: tuple[list], pos_r: int, pos_c: int) -> list:
+    """
+    Determine neighboor for one cell
+    """
+
+    amount_rows = len(matrix)
+    amount_col = len(matrix[0])
+    
+    neighboors = []
+    
+    for dir_row in range(-1, 2):
+        for dir_col in range(-1, 2):
+            if dir_row == dir_col == 0:
+                continue
+            
+            neighboor_r = dir_row + pos_r
+            neighboor_c = dir_col + pos_c
+            
+            if 0 <= neighboor_r < amount_rows and 0 <= neighboor_c < amount_col:
+                neighboors.append((neighboor_r, neighboor_c))
+                
+    return neighboors
+                
+    
+
+def tick(matrix: tuple[list]) -> tuple[list]:
+    """ 
+    Conway's game of life.
+    The following rules are applied:
+    - Any live cell with two or three live neighboors lives on
+    - Any dead cell with exactly three live neighboors becomes a live cell
+    - All other cells die or stay dead    
+    """
+    
+    for row in range(len(matrix)):
+        for column in range(len(matrix[row])):
+            neighboors = determine_neighboors(matrix, row, column)
+            
+            alive_neighboors = 0
+            for neig in neighboors:
+                if matrix[neig[0]][neig[1]] == 1:
+                    alive_neighboors += 1
+            
+            if matrix[row][column] == 1:
+                if alive_neighboors == 2 or alive_neighboors == 3:
+                    matrix[row][column] = 1
+                else:
+                    matrix[row][column] = 0
+                
+            else:
+                if matrix[row][column] == 0 and alive_neighboors == 3:
+                    matrix[row][column] = 1
+                
+    return matrix
+            
+            
+    
