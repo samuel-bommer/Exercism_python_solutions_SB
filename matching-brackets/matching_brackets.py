d317dbb5b33b5650ddd7e8977c91b4f6d923d315
samuel-bommer <samuel.bommer@gmail.com>
2026-08-12 12:27:18 +0200

    brackets

--- matching-brackets/matching_brackets.py
+++ matching-brackets/matching_brackets.py
@@ -2,12 +2,32 @@
 matching brackets
 """
 
-
 def is_paired(input_string: str) -> bool:
     """
     Returns a Bool, whether or not brackets match.
     """
     
-    brackets_string = [for char in input_string in ('(', ')', '[',']', '{','}')]
-    for index, bracket in enumerate(brackets_string):
-        pass
+    dict_brackets = {
+        '{' : '}',
+        '[' : ']',
+        '(' : ')'
+    }
+    
+    brackets = [char for char in input_string if char in ('(', ')', '[',']', '{','}')]
+    
+    if len(brackets) % 2 == 1:
+        return False
+    
+    open_brackets = []
+    for bracket in brackets:
+        if bracket in dict_brackets.keys():
+            open_brackets.append(bracket)
+        
+        if open_brackets:
+            if dict_brackets[open_brackets[-1]] == bracket:
+                open_brackets.pop(-1)
+            
+    if not open_brackets:
+        return True
+    
+    return False
